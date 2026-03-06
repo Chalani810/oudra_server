a74b20addad91637d3a90f611b1de9d23ef37b7b
WijesingheAGT <thathsaraniwijesinghe2001@gmail.com>
2026-03-06 16:15:49 +0530

    Changed the Image size limit to 10MB

--- server.js
+++ server.js
@@ -51,8 +51,9 @@ app.use(cors({
   origin: "*",
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // MongoDB connection
 const MONGO_URI =
